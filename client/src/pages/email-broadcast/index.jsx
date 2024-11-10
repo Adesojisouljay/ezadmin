07e5f2f768573938a478ff6d047402346d328bcb
Adesojisouljay <adesojiisrael@gmail.com>
2024-11-10 17:54:35 +0100

    update email input with rich editor

--- client/src/pages/email-broadcast/index.jsx
+++ client/src/pages/email-broadcast/index.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
+import ReactQuill from 'react-quill'; 
+import 'react-quill/dist/quill.snow.css';
 import { sendEmailBroadcast } from 'api';
 import './email.css';
 
 const EmailBroadcast = () => {
-
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [statusMessage, setStatusMessage] = useState('');
+  const [statusType, setStatusType] = useState('');
 
   const handleSubjectChange = (e) => {
     setSubject(e.target.value);
   };
 
-  const handleMessageChange = (e) => {
-    setMessage(e.target.value);
+  const handleMessageChange = (value) => {
+    setMessage(value);
   };
 
   const handleSendEmail = async () => {
     if (!subject || !message) {
       setStatusMessage('Please fill in both subject and message fields.');
+      setStatusType('error');
       return;
     }
 
@@ -29,8 +32,12 @@ const EmailBroadcast = () => {
     try {
       await sendEmailBroadcast(subject, message);
       setStatusMessage('Broadcast email sent successfully!');
+      setStatusType('success');
+      setMessage("")
+      setSubject("")
     } catch (error) {
       setStatusMessage('Error sending email. Please try again.');
+      setStatusType('error');
     } finally {
       setLoading(false);
     }
@@ -53,12 +60,23 @@ const EmailBroadcast = () => {
 
       <div className="input-group">
         <label htmlFor="message">Email Message</label>
-        <textarea
-          id="message"
+        <ReactQuill
           value={message}
           onChange={handleMessageChange}
           placeholder="Enter email message"
-          rows="8"
+          className='quill'
+          theme="snow"
+          modules={{
+            toolbar: [
+              [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+              [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+              ['bold', 'italic', 'underline'],
+              [{ 'align': [] }],
+              ['link', 'image'],
+              ['blockquote', 'code-block'],
+              ['clean'],
+            ],
+          }}
         />
       </div>
 
@@ -71,7 +89,7 @@ const EmailBroadcast = () => {
       </button>
 
       {statusMessage && (
-        <p className="status-message">
+        <p className={`status-message ${statusType}`}>
           {statusMessage}
         </p>
       )}
